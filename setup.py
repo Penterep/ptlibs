3ea86cd2e664a2858e4394987a1c057ad4344996
dkummel <36062569+dkummel@users.noreply.github.com>
2023-07-21 12:40:12 +0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author="Penterep",
     author_email="info@penterep.com",
     url="https://www.penterep.com/",
-    version="1.0.0",
+    version="1.0.1",
     license="GPLv3+",
     packages=setuptools.find_packages(),
     classifiers=[
